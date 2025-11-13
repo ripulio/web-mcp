cb0792ea68425fe1b181f7ab07a0c773bdd6662b
eoghan <eoghan.firth@gmail.com>
2025-11-13 21:20:33 +0000

    specify root in vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,9 @@
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  root: dirname(fileURLToPath(import.meta.url)),
   test: {
 		projects: [
 			{
